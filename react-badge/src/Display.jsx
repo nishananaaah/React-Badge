3f717af9cae9ffc230f69700443306fd32daa27a
nishananaaah <nishanaf731@gmail.com>
2024-10-04 09:27:08 +0530

    first commit

---
+++ react-badge/src/Display.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+export default function Display() {
+    const [value,setValue]=useState("nishana")
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ react-badge/src/Input.jsx
@@ -0,0 +1,14 @@
+import React, { createContext } from 'react'
+
+
+function Input() {
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Input
+
