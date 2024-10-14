5a3b1bc1d98c4403d5f18ec133e6b6e31a25ef20
nishananaaah <nishanaf731@gmail.com>
2024-10-14 09:37:53 +0530

    usenavigate

--- react-badge/src/Practices/Router/Home.jsx
+++ react-badge/src/Practices/Router/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate=useNavigate()
   return (
     <div>
      Home 
-     
+     <button onClick={()=>navigate('/about')}>about</button>
     </div>
   )
 }
